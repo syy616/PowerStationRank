b9575c944b0d132ff8af74d846bcde90220d0fb5
syy616 <shiyuanyuan616@gmail.com>
2020-03-03 17:23:37 +0800

    OK

--- js/stationrRank.js
+++ js/stationrRank.js
@@ -1,63 +1,56 @@
 var currentStation = "";
 var stationDict = {};
 var myChart = echarts.init(document.getElementById('main'));
-var nowyear=new Date().getFullYear();
-var nowmonth=new Date().getMonth()+1;
+var nowyear = new Date().getFullYear();
+var nowmonth = new Date().getMonth() + 1;
+var url = "http://192.168.1.82:7878";
 $(document).ready(function () {
+  $("#main").css("display", "none");
   DateIn(); //日期控件初始化
-  JsonData(); //给电站及年份下拉框赋值
-  InitChart();  //初始化图表查询
+  toar();   //提示框控件设置
+  ChangeYear();//年份赋值
+  GetStation(); //给电站及年份下拉框赋值
+  ChangeStation(); //根据类型切换电站
   CheckInfo();  //点击条件查询图表
-  GetRank (nowyear,nowmonth); //初始化排行榜
+  GetRank(nowyear, nowmonth, 1); //初始化排行榜
   CheckRank();  //点击条件查询图表
-  GetYesData (); //获取昨日充电排行
+  GetYesData(); //获取昨日充电排行
 });
-  //日期控件的初始化
-  function DateIn(){
+
+//日期控件的初始化
+function DateIn () {
   $('#datachange').datetimepicker({
     format: 'YYYY-MM',
     locale: moment.locale('zh-cn'),
     defaultDate: new Date(),
-  }).on('change',function(){
+  }).on('change', function () {
     alert($('#datachange').value())
   })
 }
 
-//整合Json数据并赋值给组件
-function JsonData () {
-  //请求数据给下拉框赋值
-  $.ajax({
-    type: "POST",
-    // url: url + '',
-    dataType: "JSON",
-    headers: {
-   
-    },
-    data: {
+//弹出框插件初始化
+function toar () {
 
-    },
-    success: function (response) {
-      if (response.isSuccess) {
-        var res = response.body;//接口返回的内容，body里面是请求的内容                    
-        if (res.length > 0) {
-          var dom = " ";
-          for (var i = 0; i < res.length; i++) {
-            $(".stationListSelect").append(dom);
-            dom += '<option value="' + res[i].stationId + '">' + res[i].stationName + '</option>';
-          }
-          $(".stationListSelect").append(dom);
-        }
-      }
-      else {
-        alert("服务器请求数据失败！");
-      }
-    },
-    error: function () {
-      alert("与服务连接失败！");
-    }
-  })
+  toastr.options = {
+    closeButton: false,                                            // 是否显示关闭按钮，（提示框右上角关闭按钮）
+    debug: false,                                                    // 是否使用deBug模式
+    progressBar: true,                                            // 是否显示进度条，（设置关闭的超时时间进度条）
+    positionClass: "toast-top-center",              // 设置提示款显示的位置
+    onclick: null,                                                     // 点击消息框自定义事件 
+    showDuration: "300",                                      // 显示动画的时间
+    hideDuration: "1000",                                     //  消失的动画时间
+    timeOut: "1500",                                             //  自动关闭超时时间 
+    extendedTimeOut: "1000",                             //  加长展示时间
+    showEasing: "swing",                                     //  显示时的动画缓冲方式
+    hideEasing: "linear",                                       //   消失时的动画缓冲方式
+    showMethod: "fadeIn",                                   //   显示时的动画方式
+    hideMethod: "fadeOut"                                   //   消失时的动画方式
+  };
 
-  //给年份下拉框赋值
+}
+
+//年份赋值
+function ChangeYear () {
   var year = new Date().getFullYear();
   var yearhtml = "";
   for (var i = 1; i < 4; i++) {
@@ -67,52 +60,144 @@ function JsonData () {
   $(".fullYear").append(yearhtml);
 }
 
-//初始化图表
-function InitChart () {
-  var StationId= $(".stationListSelect")[0].value;
-  var SearchType= $(".searctype")[0].value;
-  var Year=$(".fullYear")[0].value;
+//根据类型请求电站赋值给组件
+function GetStation () {
+  //请求数据给电站下拉框赋值
+  var type = $("#searctype")[0].value;
   $.ajax({
     type: "POST",
-    // url: url + '',
-    dataType: "JSON",
+    url: url + '/StationRelevanceManage/GetStationListData',
+    dataType: "json",
     headers: {
-      // token: token
+      token: "123"
     },
     data: {
-      StationId:StationId,
-      SearchType:SearchType,
-      Year:Year
+      type: type
+    },
+    success: function (response) {
+      if (response.isSuccess) {
+        var res = response.body;//接口返回的内容，body里面是请求的内容                            
+        if (res.length > 0) {
+          $("#stationListSelect").empty();
+          var dom = " ";
+          for (var i = 0; i < res.length; i++) {
+            dom += '<option value="' + res[i].indexId + '">' + res[i].stationName + '</option>';
+          }
+          $("#stationListSelect").append(dom);
+          //使用refresh方法更新UI以匹配新状态。
+          $('#stationListSelect').selectpicker('refresh');
+          //render方法强制重新渲染引导程序 - 选择ui。
+          $('#stationListSelect').selectpicker('render');
+        }
+        GetTotalDl();
+        InitChart();  //初始化图表查询
+      }
+      else {
+        toastr.error(response.errorMessage)
+      }
+    },
+    error: function () {
+      toastr.warning("与服务连接失败！");
+    }
+  })
+
+}
+
+//点击按钮切换电站
+function ChangeStation () {
+  $("#searctype").on('changed.bs.select', function (e) {
+    GetStation();
+  })
+  //点击电站自动show柱状图
+  // $("#stationListSelect").on('changed.bs.select', function (e) {
+  //   GetTotalDl();
+  //   InitChart();
+  // })
+}
+
+//根据电站Id获取总电量和总金额
+function GetTotalDl () {
+  var StationId = $("#stationListSelect")[0].value;
+  var year = $(".fullYear")[0].value;
+  $.ajax({
+    type: "POST",
+    url: url + '/StationRelevanceManage/GetStationTotalDLByYearStationIdResult',
+    dataType: "json",
+    headers: {
+      token: "123"
+    },
+    data: {
+      StationId: StationId,
+      year: year
+    },
+    success: function (response) {
+      if (response.isSuccess) {
+        var res = response.body;//接口返回的内容，body里面是请求的内容          
+        var power = Number(res.totalDL.toFixed(2));
+        var cost = Number(res.allFee.toFixed(2));
+        $(".allPower").val(convertToBigString(power));//总电量
+        $(".allCost").val(convertMoney(cost, ' 元', true));//总金额                  
+      }
+      else {
+        toastr.error(response.errorMessage)
+      }
+    },
+    error: function () {
+      toastr.warning("与服务连接失败！");
+    }
+  })
+}
+
+//初始化图表
+function InitChart () {
+  var StationId = $("#stationListSelect")[0].value;
+  var Year = $(".fullYear")[0].value;
+  $.ajax({
+    type: "POST",
+    url: url + '/StationRelevanceManage/GetStationMonthTotalDLByYearList',
+    dataType: "json",
+    headers: {
+      token: "123"
+    },
+    data: {
+      StationId: StationId,
+      Year: Year
     },
     success: function (response) {
       if (response.isSuccess) {
         var data = response.body;
         if (data.length == 0) {
-          $("#main").css("display", "block");
+          $("#main").css("display", "none");
         }
         else {
-          $("#main").css("display", "none");
-          var power = Number(data.totalDL.toFixed(2));
-          var cost = Number(data.totalMoney.toFixed(2));
-          $(".allPower").html(convertToBigString(power));//总电量
-          $(".allCost").html(convertMoney(cost, '元', true));//总金额
-          // 指定图表的配置项和数据
+          $("#main").css("display", "block");
+          var list = data.totalDlList;
           var everydate = [];
-          var moneyY = [];
-          $.each(data, function (index, item) {
-            moneyY.push(item.consumePrice.toString());
-          });
+          var PowerY = [];
+          for (var m = 0; m < list.length; m++) {
+            everydate.push(list[m].monthNum);
+            PowerY.push(list[m].totalDL);
+          }
+          // 指定图表的配置项和数据
+          // $.each(data, function (index, item) {
+          //   moneyY.push(item.consumePrice.toString());
+          // });
           var option = {
             title: {
               text: '电站每月电量'
             },
-            tooltip: {},
+            tooltip: {
+              show: true,
+              confine: true, //将提示框放在图表内
+              formatter: '日期:{b} 月<br/>充电电量:{c} kWh',
+            },
             legend: {
-              data: ['电量', '费用']
+              data: ['电量']
             },
             xAxis: {
+              name: '单位:月份',
               triggerEvent: true,
-              data: []
+              data: everydate
             },
             yAxis: {
               triggerEvent: true,
@@ -126,10 +211,13 @@ function InitChart () {
                     show: false,
                     position: 'top'
                   },
+                  itemStyle: {
+                    color: '#5bc0de'
+                  },
                   barMaxWidth: 40,
                   name: '电量',
                   type: 'bar',
-                  data: moneyY
+                  data: PowerY
                 }
               ]
           };
@@ -137,7 +225,7 @@ function InitChart () {
           myChart.setOption(option);
           myChart.on('click', function (params) {
             console.log(params.name);
-            var stationName = $(".stationListSelect").val();
+            var stationName = $("#stationListSelect").val();
             var year = $(".fullYear").value();
             var month = params.name;
             location.href = "http://www.ba.com?stationName=" + stationName + "&year=" + year + "&month=" + month;
@@ -159,86 +247,103 @@ function InitChart () {
   })
 }
 
-//点击请求图表数据
+//点击按钮请求图表数据
 function CheckInfo () {
   $("#checkinfo").on('click', function () {
     InitChart();
+    GetTotalDl();
   })
 }
 
 //初始查询当前月排名
-function GetRank(year,month){
-    $.ajax({
-      type: "POST",
-      url: url + "",
-      data: {
-        Year: year,
-        Month: month
-      },
-      dataType: "json",
-      beforeSend: function (request) {
-        request.setRequestHeader("token", token);
-      },
-      success: function (response) {
-        if (response.isSuccess) {
-          var list = response.body.list;
-          if (list.length > 0) that.orderTableData = list;
-          var rankPanel = $("#rankList");
-          var trtmp = "";
+function GetRank (year, month, type) {
+  var rankPanel = $("#rankList");
+  var trtmp = "";
+  $.ajax({
+    type: "POST",
+    url: url + "/StationRelevanceManage/GetAllStationTotalDByYearMonthList",
+    data: {
+      pageIndex: 1,
+      pageCount: 10,
+      year: year,
+      Month: month,
+      Type: type
+    },
+    dataType: "json",
+    success: function (response) {
+      if (response.isSuccess) {
+        var list = response.body.list;
+        if (list.length > 0) {
           for (var i = 0; i < list.length; i++) {
-            trtmp += ("<tr><td class='stationId'>" + data.id + "</td><td class='station'>" + data[i].name + "</td>" +
-              "<td class='power'>" + data[i].power + "</td></tr>");
+            trtmp += ("<tr><td class='stationId'>" + Number(i + 1) + "</td><td class='station'>" + list[i].stationName + "</td>" +
+              "<td class='power'>" + convertToBigString(list[i].totalDL) + "</td></tr>");
           }
           rankPanel.html(trtmp);
         }
-      },
-      error: function (jqXHR) {
-
+        else {
+          trtmp = ("<tr><td class='stationId'></td><td class='station'>暂无数据~</td><td class='power'></td></tr>")
+          rankPanel.html(trtmp);
+          toastr.error("暂无数据")
+        }
       }
-    });
- 
+    },
+    error: function (jqXHR) {
+      trtmp = ("<tr><td class='stationId'></td><td class='station'>暂无数据~</td><td class='power'></td></tr>")
+      rankPanel.html(trtmp);
+      toastr.error("暂无数据")
+      toastr.warning("与服务连接失败！");
+    }
+  });
+
 }
 
 //点击查询当前月排名
-function CheckRank(){
-  $("#checkrank").on('click',function(){
-  // alert($("#datachange")[0].value);
-  var thisdate=$("#datachange")[0].value.split("-");
-  year=thisdate[0];
-  month=thisdate[1];
-  GetRank (year,month);
+function CheckRank () {
+  $("#checkrank").on('click', function () {
+    // alert($("#datachange")[0].value);
+    var thisdate = $("#datachange")[0].value.split("-"),
+      year = thisdate[0],
+      month = thisdate[1],
+      type = $(".searctype2")[0].value;
+    GetRank(year, month, type);
   })
 }
 
 //获取昨日充电排行
 function GetYesData () {
+  var type = $(".searctype2")[0].value;
+  var rankYesPanel = $("#rankYesList");
+  var trtmp = "";
   $.ajax({
     type: "POST",
-    url: baseURL + "/Order/GetOrderAllData",
+    url: url + "/StationRelevanceManage/GetAllStationTotalDByYesterdayList",
     data: {
+      pageIndex: 1,
+      pageCount: 10,
+      Type: type
     },
     dataType: "json",
-    beforeSend: function (request) {
-      request.setRequestHeader("token", token);
-    },
     success: function (response) {
       if (response.isSuccess) {
-        var list = response.body.list;
-        if (list.length > 0) that.orderTableData = list;
-        var rankYesPanel = $("#rankYesList");
-        var trtmp = "";
-        for (var i = 0; i < list.length; i++) {
-          trtmp += ("<tr><td class='stationId'>" + data.id + "</td><td class='station'>" + data[i].name + "</td>" +
-            "<td class='power'>" + data[i].power + "</td></tr>");
+        var list = response.body.list; {
+          for (var i = 0; i < list.length; i++) {
+            trtmp += ("<tr><td class='stationId'>" + Number(i + 1) + "</td><td class='station'>" + list[i].stationName + "</td>" +
+              "<td class='power'>" + convertToBigString(list[i].totalDL) + "</td></tr>");
+          }
         }
         rankYesPanel.html(trtmp);
       }
       else {
-
+        trtmp = ("<tr><td class='stationId'></td><td class='station'>暂无数据~</td><td class='power'></td></tr>")
+        rankPanel.html(trtmp);
+        toastr.error("暂无数据")
       }
     },
     error: function (jqXHR) {
-
+      trtmp = ("<tr><td class='stationId'></td><td class='station'>暂无数据~</td><td class='power'></td></tr>")
+      rankPanel.html(trtmp);
+      toastr.error("暂无数据")
+      toastr.warning("与服务连接失败！");
     }
   });
 
@@ -271,27 +376,27 @@ function GetYesData () {
 //单位转换
 function convertToBigString (val) {
   if (val > 1000 * 1000 * 10) {
-    return (val / (1000 * 1000)).toFixed(2) + 'GWh';
+    return (val / (1000 * 1000)).toFixed(2) + ' GWh';
   }
   if (val > 1000 * 10) {
-    return (val / 1000).toFixed(2) + 'MWh';
+    return (val / 1000).toFixed(2) + ' MWh';
   }
-  return (val).toFixed(2) + 'KWh';
+  return (val).toFixed(2) + ' KWh';
 }
 
 function convertMoney (val, preix, isRemain) {
   if (val > 10000 * 1000) {
 
     if (isRemain) {
-      return (val / (10000 * 1000)).toFixed(2) + '千万元';
+      return (val / (10000 * 1000)).toFixed(2) + ' 千万元';
     }
-    return (val / (10000 * 1000)).toFixed(0) + '千万元';
+    return (val / (10000 * 1000)).toFixed(0) + ' 千万元';
   }
   if (val > 10000 * 10) {
     if (isRemain) {
-      return (val / 10000).toFixed(2) + '万元';
+      return (val / 10000).toFixed(2) + ' 万元';
     }
-    return (val / 10000).toFixed(0) + '万元';
+    return (val / 10000).toFixed(0) + ' 万元';
   }
   if (isRemain) {
     return (val).toFixed(2) + preix;
